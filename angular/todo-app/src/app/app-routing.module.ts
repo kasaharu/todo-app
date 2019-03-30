49d88f24cddf9ab63382a8540734d1b848aa2b3d
Wataru.Kasahara <kasahara1019@gmail.com>
2019-03-30 21:33:23 +0900

    ルーティングの設定

--- angular/todo-app/src/app/app-routing.module.ts
+++ angular/todo-app/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { TodoListComponent } from './pages/todo-list/todo-list.component';
+
+const routes: Routes = [{ path: '', redirectTo: '/todo-list', pathMatch: 'full' }, { path: 'todo-list', component: TodoListComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
