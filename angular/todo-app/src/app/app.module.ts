b3c9c921a8ecda7b848a27aa516d10103cfd4a8c
Wataru.Kasahara <kasahara1019@gmail.com>
2019-03-30 22:08:16 +0900

    Generate HomeComponent by CLI command

--- angular/todo-app/src/app/app.module.ts
+++ angular/todo-app/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListPageComponent } from './pages/todo-list/todo-list.component';
 import { TodoListComponent } from './containers/todo-list/todo-list.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, TodoListPageComponent, TodoListComponent],
+  declarations: [AppComponent, TodoListPageComponent, TodoListComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
