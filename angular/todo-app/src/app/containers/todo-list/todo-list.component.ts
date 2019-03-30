4f632ec208a490344146aae4723eee27f37628dc
Wataru.Kasahara <kasahara1019@gmail.com>
2019-03-30 22:41:30 +0900

    Todo のダミーデータを作成

--- angular/todo-app/src/app/containers/todo-list/todo-list.component.ts
+++ angular/todo-app/src/app/containers/todo-list/todo-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { Todo } from '../../core/domains';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
+  todoList$: Observable<Todo[]>;
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.todoList$ = of([
+      { id: 1, title: 'title 1', isCompleted: false },
+      { id: 2, title: 'title 2', isCompleted: false },
+      { id: 3, title: 'title 3', isCompleted: false },
+    ]);
+  }
 }
