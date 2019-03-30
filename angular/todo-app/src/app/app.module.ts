a8454c004ff0e76d98cbfb90865530cc7a33bae3
Wataru.Kasahara <kasahara1019@gmail.com>
2019-03-30 22:10:54 +0900

    不要になった page component を削除

--- angular/todo-app/src/app/app.module.ts
+++ angular/todo-app/src/app/app.module.ts
@@ -3,12 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoListPageComponent } from './pages/todo-list/todo-list.component';
 import { TodoListComponent } from './containers/todo-list/todo-list.component';
 import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, TodoListPageComponent, TodoListComponent, HomeComponent],
+  declarations: [AppComponent, TodoListComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
