4c3dd8f829488d1f079f46930d8d55809836475b
Wataru.Kasahara <kasahara1019@gmail.com>
2019-03-30 21:29:18 +0900

    Generate TodoListComponent by CLI command

--- angular/todo-app/src/app/app.module.ts
+++ angular/todo-app/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './pages/todo-list/todo-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TodoListComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
