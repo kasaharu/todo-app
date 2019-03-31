359e3f0686e13148762d712c115cadb369406ec6
Wataru.Kasahara <kasahara1019@gmail.com>
2019-03-31 14:50:33 +0900

    TodoService で BehaviorSubject を使用した状態管理の仕組みを実装

--- angular/todo-app/src/app/services/todo.service.ts
+++ angular/todo-app/src/app/services/todo.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Todo } from '../core/domains';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
+  _todoList$ = new BehaviorSubject<Todo[]>([]);
+
+  get todoList$() {
+    return this._todoList$;
+  }
+
   constructor() {}
+
+  fetchTodoList() {
+    const nextTodoList = [
+      { id: 1, title: 'title 1', isCompleted: false },
+      { id: 2, title: 'title 2', isCompleted: false },
+      { id: 3, title: 'title 3', isCompleted: false },
+    ];
+
+    this._todoList$.next(nextTodoList);
+  }
 }
