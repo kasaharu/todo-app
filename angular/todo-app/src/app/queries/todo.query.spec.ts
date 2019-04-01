023b83a857c4f8a5f1fc06654f674efe7fe06fe3
Wataru.Kasahara <kasahara1019@gmail.com>
2019-04-01 20:12:16 +0900

    Rename TodoQueryService -> TodoQuery

--- angular/todo-app/src/app/queries/todo.query.spec.ts
+++ angular/todo-app/src/app/queries/todo.query.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { TodoQueryService } from './todo-query.service';
+import { TodoQuery } from './todo.query';
 
-describe('TodoQueryService', () => {
+describe('TodoQuery', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: TodoQueryService = TestBed.get(TodoQueryService);
+    const service: TodoQuery = TestBed.get(TodoQuery);
     expect(service).toBeTruthy();
   });
 });
