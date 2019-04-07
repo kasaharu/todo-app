204f82877485b4abb2e2414ae3a46f5bc53d9d26
Wataru.Kasahara <kasahara1019@gmail.com>
2019-04-07 21:18:21 +0900

    TodoUsecase の initialize() のテストを実装

--- angular/todo-app/src/app/usecases/todo.usecase.spec.ts
+++ angular/todo-app/src/app/usecases/todo.usecase.spec.ts
@@ -1,7 +1,27 @@
+import { TestBed } from '@angular/core/testing';
+
 import { TodoUsecase } from './todo.usecase';
+import { TodoService } from '../services/todo.service';
+
+export class MockTodoService {
+  fetchTodoList() {}
+}
 
 describe('TodoUsecase', () => {
-  it('should create an instance', () => {
-    expect(new TodoUsecase()).toBeTruthy();
+  let usecase: TodoUsecase;
+  let todoService: TodoService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [{ provide: TodoService, useClass: MockTodoService }],
+    });
+    usecase = TestBed.get(TodoUsecase);
+    todoService = TestBed.get(TodoService);
+  });
+
+  it('call initialize()', () => {
+    spyOn(todoService, 'fetchTodoList');
+    usecase.initialize();
+    expect(todoService.fetchTodoList).toHaveBeenCalled();
   });
 });
